a049c985a548e6f3b3c58027db07afc09f19934f
Matthias <74272510+Akushini@users.noreply.github.com>
2022-05-05 21:55:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
-
-from utils import lancer_des
+from utils import lancer_des, lancer_gemme, lancer_objet_art
 
 butin = False
 while not butin:
@@ -16,7 +15,7 @@ while not butin:
     facteur_tresor = input("Entrez le facteur de tresor :")
 
     # tirage aleatoire du tresor en pieces
-    piece_cuivre = piece_argent = piece_or = piece_platine = gemmes = objets_arts = objet_non_magique = 0
+    piece_cuivre = piece_argent = piece_or = piece_platine = gemmes = objets_arts = objets_non_magiques = 0
     objet_magique_faible = objets_magique_intermediare = objets_magique_puissant = 0
 
     # tirage niveau 1
@@ -47,7 +46,7 @@ while not butin:
         while objets_tires < int(facteur_tresor):
             resultat = random.randint(1, 100)
             if 75 <= resultat <= 95:
-                objet_non_magique += 1
+                objets_non_magiques += 1
             if 96 <= resultat <= 100:
                 objet_magique_faible += 1
             objets_tires += 1
@@ -80,7 +79,7 @@ while not butin:
         while objets_tires < int(facteur_tresor):
             resultat = random.randint(1, 100)
             if 50 <= resultat <= 85:
-                objet_non_magique += 1
+                objets_non_magiques += 1
             if 86 <= resultat <= 100:
                 objet_magique_faible += 1
             objets_tires += 1
@@ -113,7 +112,7 @@ while not butin:
         while objets_tires < int(facteur_tresor):
             resultat = random.randint(1, 100)
             if 50 <= resultat <= 79:
-                objet_non_magique += lancer_des(1, 3)
+                objets_non_magiques += lancer_des(1, 3)
             if 80 <= resultat <= 100:
                 objet_magique_faible += 1
             objets_tires += 1
@@ -146,7 +145,7 @@ while not butin:
         while objets_tires < int(facteur_tresor):
             resultat = random.randint(1, 100)
             if 43 <= resultat <= 62:
-                objet_non_magique += lancer_des(1, 4)
+                objets_non_magiques += lancer_des(1, 4)
             if 63 <= resultat <= 100:
                 objet_magique_faible += 1
             objets_tires += 1
@@ -179,78 +178,51 @@ while not butin:
         while objets_tires < int(facteur_tresor):
             resultat = random.randint(1, 100)
             if 58 <= resultat <= 67:
-                objet_non_magique += lancer_des(1, 4)
+                objets_non_magiques += lancer_des(1, 4)
             if 68 <= resultat <= 100:
                 objet_magique_faible += lancer_des(1, 3)
             objets_tires += 1
 
     # Tirage de la valeur des gemmes
+    list_gemmes = []
     valeur_des_gemmes = 0
     gemmes_estimees = 0
     while gemmes_estimees < gemmes:
-        resultat = random.randint(1, 100)
-        if 1 <= resultat <= 25:
-            valeur_des_gemmes += 10
-        if 26 <= resultat <= 50:
-            valeur_des_gemmes += 50
-        if 51 <= resultat <= 70:
-            valeur_des_gemmes += 100
-        if 71 <= resultat <= 90:
-            valeur_des_gemmes += 500
-        if 91 <= resultat <= 99:
-            valeur_des_gemmes += 1000
-        if resultat == 100:
-            valeur_des_gemmes += 5000
+        valeur_des_gemmes, valeur, nom_gemme = lancer_gemme(valeur_des_gemmes)
+        list_gemmes.append((nom_gemme, valeur))
         gemmes_estimees += 1
 
     # Tirage de la valeur des objets d'art
+    list_objets_arts = []
     valeur_des_objets_arts = 0
     objets_arts_estimees = 0
     while objets_arts_estimees < objets_arts:
-        resultat = random.randint(1, 100)
-        if 1 <= resultat <= 10:
-            valeur_des_objets_arts += 55
-        if 11 <= resultat <= 25:
-            valeur_des_objets_arts += 105
-        if 26 <= resultat <= 40:
-            valeur_des_objets_arts += 350
-        if 41 <= resultat <= 50:
-            valeur_des_objets_arts += 550
-        if 51 <= resultat <= 60:
-            valeur_des_objets_arts += 700
-        if 61 <= resultat <= 70:
-            valeur_des_objets_arts += 1050
-        if 71 <= resultat <= 80:
-            valeur_des_objets_arts += 1400
-        if 81 <= resultat <= 85:
-            valeur_des_objets_arts += 1750
-        if 86 <= resultat <= 90:
-            valeur_des_objets_arts += 2500
-        if 91 <= resultat <= 95:
-            valeur_des_objets_arts += 3500
-        if 96 <= resultat <= 99:
-            valeur_des_objets_arts += 5000
-        if resultat == 100:
-            valeur_des_objets_arts += 7000
+        valeur_des_objets_arts, valeur, nom_objet_art = lancer_objet_art(valeur_des_objets_arts)
+        list_objets_arts.append((nom_objet_art, valeur))
         objets_arts_estimees += 1
-
-
+    print()
     print(f'Piece de Cuivre : {piece_cuivre}')
     print(f'Piece d\'argent : {piece_argent}')
     print(f'Piece d\'or : {piece_or}')
     print(f'Piece de platine : {piece_platine}')
-
-    print(f'Gemmes : {gemmes}')
-    print(f'Valeur des gemmes récupérées : {valeur_des_gemmes} pièces d\'or')
-
-    print(f'Objets d\'arts : {objets_arts}')
-    print(f'Valeurs des objets d\'arts récupérés : {valeur_des_objets_arts} pièces d\'or')
-
-    print(f'Objets communs : {objet_non_magique}')
+    print()
+    print(f'Gemmes trouvées : {gemmes}')
+    for aTuple in list_gemmes:
+        print('%s d\'une valeur de %s pièces d\'or' % aTuple)
+    print()
+    print(f'Valeur totale des gemmes récupérées : {valeur_des_gemmes} pièces d\'or')
+    print()
+    print(f'Objets d\'arts trouvés : {objets_arts}')
+    for aTuple in list_objets_arts:
+        print('%s d\'une valeur de %s pièces d\'or' % aTuple)
+    print()
+    print(f'Valeurs totale des objets d\'arts récupérés : {valeur_des_objets_arts} pièces d\'or')
+    print()
+    print(f'Objets communs : {objets_non_magiques}')
     print(f'Objets magiques faibles : {objet_magique_faible}')
     print(f'Objets magiques intermédiaires : {objets_magique_intermediare}')
     print(f'Objets magiques puissants : {objets_magique_puissant}')
-
+    print()
     reponse_finale = False
     while not reponse_finale:
         recommencer = input("Voulez-vous tirer un autre butin ? :")
@@ -262,4 +234,4 @@ while not butin:
             reponse_finale = True
             print("Au revoir")
         else:
-            print("Repondez par oui ou non")
+            print("Répondez par oui ou non")
